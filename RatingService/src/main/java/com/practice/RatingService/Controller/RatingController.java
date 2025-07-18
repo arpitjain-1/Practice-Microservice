ff9a625f3b0ab1f5df4d9fda28fca9873566f8c1
Arpit Jain <arpitjainkhi56@gmail.com>
2025-07-18 16:40:20 +0530

    Completed Rating-service

---
+++ RatingService/src/main/java/com/practice/RatingService/Controller/RatingController.java
@@ -0,0 +1,40 @@
+package com.practice.RatingService.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.practice.RatingService.Entity.Rating;
+import com.practice.RatingService.Service.RatingService;
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingController {
+
+    @Autowired
+    private RatingService ratingService;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createRating(@RequestBody Rating rating) {
+        return ResponseEntity.ok(ratingService.createRating(rating));
+    }
+
+    @GetMapping("/hotel/{id}")
+    public ResponseEntity<List<Rating>> getRatingsByHotelId(@PathVariable String id) {
+        return ResponseEntity.ok(ratingService.getRatingsByHotelId(id));
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<List<Rating>> getRatingsByUserId(@PathVariable String id) {
+        return ResponseEntity.ok(ratingService.getRatingsByUserId(id));
+    }
+}
+

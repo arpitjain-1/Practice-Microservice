1832027dd305ea98427ba4a49d07d549c9e32b34
Arpit Jain <arpitjainkhi56@gmail.com>
2025-07-24 19:15:17 +0530

    Remove Extras

--- RatingService/src/main/java/com/practice/RatingService/Controller/RatingController.java
+++ RatingService/src/main/java/com/practice/RatingService/Controller/RatingController.java
@@ -1,7 +1,6 @@
 package com.practice.RatingService.Controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
